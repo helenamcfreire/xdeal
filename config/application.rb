c27b6c8fa0f036f58a4307be8ea88f2855b5d457
helenamcfreire <helenamcfreire@gmail.com>
2015-04-25 19:24:41 -0300

    first commit

--- config/application.rb
+++ config/application.rb
@@ -12,8 +12,8 @@ end
 module Xdeal
   class Application < Rails::Application
 
-    #To Heroku
-    #config.assets.initialize_on_precompile = false
+    #Heroku
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
